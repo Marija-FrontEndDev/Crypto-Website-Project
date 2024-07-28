945a04e770dc6d962f6d5200b5ed4f666dd827cf
Marija Mitrovic <marija.mitrovic.frontend@gmail.com>
2024-07-28 18:04:55 +0200

    Add path for every page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import React from 'react'
 import Home from "@pages/Home"
 import Cryptocurrencies from "@pages/Cryptocurrencies"
 import News from "@pages/News"
+import LogIn from "@pages/LogIn"
+import GetStarted from "@pages/GetStarted"
 
 
 //components 
@@ -13,14 +15,37 @@ import NavBar from "@components/NavBar"
 //styles
 import "@styles/global.scss"
 
-//others
+//libraries
 
-const App = () => {
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+
+
+const Layout = () => {
   return (
-    <div className='app'>
+    <div className='App'>
       <NavBar/>
+      <Outlet/>
     </div>
   )
+}
+
+const router = createBrowserRouter ([
+  {path: "/", 
+    element: <Layout/> ,
+    children: [
+      {index: true, element: <Home/> },
+      {path: "/cryptocurrencies", element: <Cryptocurrencies/> },
+      {path: "/news", element: <News/> },
+      {path: "/login", element: <LogIn/> },
+      {path: "/get-started", element: <GetStarted/> },
+      
+    ]
+  }
+])
+
+
+function App () {
+  return <RouterProvider router={router} /> ;
 }
 
 export default App
