336fda8df47b45cb951d4b41441cf818d0a1e90a
Marija Mitrovic <marija.mitrovic.frontend@gmail.com>
2024-07-28 18:09:34 +0200

    Create links for every page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,27 +3,42 @@ import React from 'react'
 import "@styles/NavBar.scss"
 import logo from "@assets/logo-pic.webp"
 
+import { Link } from 'react-router-dom'
+
 const NavBar = () => {
   return (
     <div className='navbar'>
+      
+      
         <div className='logo'>
         <img src={logo} alt="" />
         <h3>CryptoWorld</h3>
+
         </div>
-        
         <ul>
-            <li>Home</li>
-            <li>Cryptocurrencies</li>
-            <li>News</li>
-            
-        </ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+        </li>
+        <li>
+          <Link to="/news">News</Link>
+        </li>
+      </ul>
+
         <div className='nav-right'>
-            <button className='login'>Log In </button>
-            <button className='start'>Get Started</button>
+            <button className='login'>
+            <Link to="/login">Log In</Link>
+            </button>
+            <button className='start'>
+            <Link to="/get-started">Get Started</Link>
+            </button>
 
         </div>
     </div>
   )
 }
+
 
 export default NavBar
