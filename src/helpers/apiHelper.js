cb62c724f2ebfd6e84bcf0dcc820354e538cf5d8
Marija Mitrovic <marija.mitrovic.frontend@gmail.com>
2024-07-29 18:22:45 +0200

    Add Api using axios and coingeckoapi

---
+++ src/helpers/apiHelper.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const allCoinsApiEndpoint = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd";
+
+export const fetchAllCoins = async () => {
+  const { data } = await axios.get(allCoinsApiEndpoint);
+  return data;
+};
+
+const singleCoinApiEndpoint= "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd/";
+
+export const fetchSingleCoin = async (id) => {
+  const { data } = await axios.get(`${singleCoinApiEndpoint}${id}`);
+  return data;
+};
+
