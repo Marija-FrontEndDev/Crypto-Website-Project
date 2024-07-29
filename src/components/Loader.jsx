0d1c0e5645951860f7adc6af57e442253256db0f
Marija Mitrovic <marija.mitrovic.frontend@gmail.com>
2024-07-29 18:36:21 +0200

    Create loader and use it in HomePage

---
+++ src/components/Loader.jsx
@@ -0,0 +1,14 @@
+import "@styles/Loader.scss";
+
+const Loader = () => {
+  return (
+    <div className="lds-ellipsis">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loader;
