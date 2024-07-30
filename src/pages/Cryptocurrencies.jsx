e80e0fda3fa2148329f0c6d937d18033330ce283
Marija Mitrovic <marija.mitrovic.frontend@gmail.com>
2024-07-30 19:46:01 +0200

    Create Cryptocurrency Page : All cryptocurrenies shown/ Put search button- to find a desired cryptocurrecy/ link every cryptocurrency, so when you click on specific cryptocurrency it will open new page with details of that one

--- src/pages/Cryptocurrencies.jsx
+++ src/pages/Cryptocurrencies.jsx
@@ -1,10 +1,81 @@
-import React from 'react'
-import "@styles/Cryptocurrencies.scss"
+import React from "react";
+import { useState } from "react";
+import "@styles/Cryptocurrencies.scss";
+import { useQuery } from "@tanstack/react-query";
+import { fetchAllCoins } from "@helpers/apiHelper.js";
+import Loader from "@components/Loader.jsx";
+import { Link } from "react-router-dom";
 
 const Cryptocurrencies = () => {
-  return (
-    <div>Cryptocurrencies</div>
-  )
-}
+  const [searchCoin, setSearchCoin] = useState("");
 
-export default Cryptocurrencies
+  const { data, error, isPending } = useQuery({
+    queryKey: ["allCoins"],
+    queryFn: fetchAllCoins,
+  });
+
+  const filteredCoins = data?.filter(
+    (coin) =>
+      coin.name.toLowerCase().includes(searchCoin.toLowerCase()) ||
+      coin.symbol.toLowerCase().includes(searchCoin.toLowerCase())
+  );
+
+  return (
+    <div>
+      <div className="searchcrypto">
+        <input
+          type="text"
+          placeholder="Search cryptocurrency..."
+          value={searchCoin}
+          onChange={(e) => setSearchCoin(e.target.value)}
+        />
+      </div>
+
+      <div className="crypto-table">
+        <div className="table-layout">
+          <p>#</p>
+          <p>Coins</p>
+          <p>Symbol</p>
+          <p>Price</p>
+          <p style={{ textAlign: "center" }}>24h Change</p>
+          <p className="market-cap">Market Cap</p>
+        </div>
+
+        <div className="table-body">
+          {error && <div> An error occured. Please refresh the page. </div>}
+          {isPending && <Loader />}
+          {filteredCoins &&
+            filteredCoins.map((coin) => (
+              <Link
+                to={`/coin/${coin.id}`}
+                key={coin.id}
+                style={{ textDecoration: "none" }}
+              >
+                <div className="table-layout" key={coin.id}>
+                  <p>{coin.market_cap_rank}</p>
+                  <div>
+                    <img src={coin.image} />
+                    <p>{coin.name}</p>
+                  </div>
+                  <p>{coin.symbol.toUpperCase()}</p>
+                  <p>${coin.current_price.toFixed(2)}</p>
+                  <p
+                    className={
+                      coin.price_change_percentage_24h > 0 ? "green" : "red"
+                    }
+                  >
+                    {Math.floor(coin.price_change_percentage_24h * 100) / 100}
+                  </p>
+                  <p className="market-cap">
+                    ${coin.market_cap.toLocaleString()}
+                  </p>
+                </div>
+              </Link>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cryptocurrencies;
