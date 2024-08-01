629f346df4464c8c1926fec4a6309eab2550d1dd
Marija Mitrovic <marija.mitrovic.frontend@gmail.com>
2024-08-01 20:06:28 +0200

    Finish coinDetails page

--- src/pages/CoinDetails.jsx
+++ src/pages/CoinDetails.jsx
@@ -7,6 +7,7 @@ import { useQuery } from '@tanstack/react-query'
 import { fetchSingleCoin } from '@helpers/apiHelper.js'
 
 import Loader from "@components/Loader.jsx"
+import CoinChart from '@pages/CoinChart.jsx'
 
 const CoinDetail = () => {
   const { id } = useParams();
@@ -22,7 +23,15 @@ const CoinDetail = () => {
       { isPending && <Loader />}
       {data &&  (
         <div>
-          <p>{data.name}</p>
+          <img src={data.image.large} alt={data.name} />
+          <h1>{data.name} ({data.symbol.toUpperCase()})</h1>
+          <p> <CoinChart /></p>
+          <p>Crypto Market Rank: {data.market_cap_rank}</p>
+          <p>Current Price: ${data.market_data.current_price.usd.toLocaleString()}</p>
+          <p>Market Cap: ${data.market_data.market_cap.usd.toLocaleString()}</p>
+          <p>24h Change: {data.market_data.price_change_percentage_24h.toLocaleString()}%</p>
+          <p>24h high: ${data.market_data.high_24h.usd.toLocaleString()}</p>
+          <p>24h low : ${data.market_data.low_24h.usd.toLocaleString()}</p>
           </div>
       )}
     </div>
