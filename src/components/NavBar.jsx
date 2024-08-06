5a6aa64947664a6f47056e4944be5b9332bc9d2c
Marija Mitrovic <marija.mitrovic.frontend@gmail.com>
2024-08-06 17:47:15 +0200

    Add MoonIcon button and DarkMode in NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,17 +3,28 @@ import React from 'react'
 import "@styles/NavBar.scss"
 import logo from "@assets/logo-pic.webp"
 
+import MoonIcon from "@assets/moon.jpg";
+
 import { Link } from 'react-router-dom'
+import { useEffect } from 'react';
+
+// global state
+import { darkModeConfig } from "@helpers/zustandDarkMode.js";
 
 const NavBar = () => {
-  return (
-    <div className='navbar'>
-      
-      
-        <div className='logo'>
-        <img src={logo} alt="" />
-        <h3>CryptoWorld</h3>
 
+  const { darkMode, setDarkMode } = darkModeConfig();
+
+  useEffect(() => {
+    localStorage.setItem("DarkTheme", JSON.stringify(darkMode));
+  }, [darkMode]);
+
+  return (
+    <nav className={darkMode ? "navbar dark" : "navbar"}>
+<div className='navbar'>
+        <div className='logo'>
+        <img src={logo} alt="" /> 
+          <Link to="/"><h3>CryptoWorld</h3></Link>
         </div>
         <ul>
         <li>
@@ -23,7 +34,7 @@ const NavBar = () => {
           <Link to="/cryptocurrencies">Cryptocurrencies</Link>
         </li>
         <li>
-          <Link to="/news">News</Link>
+          <Link to="/learn-more">Learn More</Link>
         </li>
       </ul>
 
@@ -34,9 +45,15 @@ const NavBar = () => {
             <button className='start'>
             <Link to="/get-started">Get Started</Link>
             </button>
+            <button onClick={() => setDarkMode()}>
+            <img src={MoonIcon} alt="moon icon" className="moonicon"/>
+          </button>
 
         </div>
     </div>
+
+    </nav>
+    
   )
 }
 
