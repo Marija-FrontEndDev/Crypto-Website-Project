d02caffa69003c22ed8558feeefe66681cbabc87
Marija Mitrovic <marija.mitrovic.frontend@gmail.com>
2024-08-07 13:05:05 +0200

    Add DarkMode in Cryptocurrency page

--- src/pages/Cryptocurrencies.jsx
+++ src/pages/Cryptocurrencies.jsx
@@ -5,8 +5,13 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchAllCoins } from "@helpers/apiHelper.js";
 import Loader from "@components/Loader.jsx";
 import { Link } from "react-router-dom";
+import search from "@assets/search.png"
+import { darkModeConfig } from "@helpers/zustandDarkMode.js";
 
 const Cryptocurrencies = () => {
+
+  const { darkMode, setDarkMode } = darkModeConfig();
+
   const [searchCoin, setSearchCoin] = useState("");
 
   const { data, error, isPending } = useQuery({
@@ -21,8 +26,8 @@ const Cryptocurrencies = () => {
   );
 
   return (
-    <div>
-      <div className="searchcrypto">
+    <div className={darkMode ? "Crypto dark" : "Crypto"}>
+      <div className="searchcrypto" >
         <input
           type="text"
           placeholder="Search cryptocurrency..."
@@ -30,50 +35,51 @@ const Cryptocurrencies = () => {
           onChange={(e) => setSearchCoin(e.target.value)}
         />
       </div>
-
-      <div className="crypto-table">
-        <div className="table-layout">
-          <p>#</p>
-          <p>Coins</p>
-          <p>Symbol</p>
-          <p>Price</p>
-          <p style={{ textAlign: "center" }}>24h Change</p>
-          <p className="market-cap">Market Cap</p>
+      <div className="crypto-table" >
+      <div className={darkMode ? "Crypto table" : "Crypto"}>
+<div className="table-layout" >
+  <p>#</p>
+  <p>Coins</p>
+  <p>Symbol</p>
+  <p>Price</p>
+  <p style={{ textAlign: "center" }}>24h Change</p>
+  <p className="market-cap">Market Cap</p>
+</div>
+<div className="table-body">
+  {error && <div> An error occured. Please refresh the page. </div>}
+  {isPending && <Loader />}
+  {filteredCoins &&
+    filteredCoins.map((coin) => (
+      <Link
+        to={`/coin/${coin.id}`}
+        key={coin.id}
+        style={{ textDecoration: "none" }}
+      >
+        <div className="table-layout" key={coin.id}>
+          <p>{coin.market_cap_rank}</p>
+          <div>
+            <img src={coin.image} />
+            <p>{coin.name}</p>
+          </div>
+          <p>{coin.symbol.toUpperCase()}</p>
+          <p>${coin.current_price.toLocaleString()}</p>
+          <p
+            className={
+              coin.price_change_percentage_24h > 0 ? "green" : "red"
+            }
+          >
+            {Math.floor(coin.price_change_percentage_24h * 100) / 100}%
+          </p>
+          <p className="market-cap">
+            ${coin.market_cap.toLocaleString()}
+          </p>
         </div>
-
-        <div className="table-body">
-          {error && <div> An error occured. Please refresh the page. </div>}
-          {isPending && <Loader />}
-          {filteredCoins &&
-            filteredCoins.map((coin) => (
-              <Link
-                to={`/coin/${coin.id}`}
-                key={coin.id}
-                style={{ textDecoration: "none" }}
-              >
-                <div className="table-layout" key={coin.id}>
-                  <p>{coin.market_cap_rank}</p>
-                  <div>
-                    <img src={coin.image} />
-                    <p>{coin.name}</p>
-                  </div>
-                  <p>{coin.symbol.toUpperCase()}</p>
-                  <p>${coin.current_price.toFixed(2)}</p>
-                  <p
-                    className={
-                      coin.price_change_percentage_24h > 0 ? "green" : "red"
-                    }
-                  >
-                    {Math.floor(coin.price_change_percentage_24h * 100) / 100}
-                  </p>
-                  <p className="market-cap">
-                    ${coin.market_cap.toLocaleString()}
-                  </p>
-                </div>
-              </Link>
-            ))}
-        </div>
-      </div>
+      </Link>
+    ))}
+</div>
+</div>
+</div>
+      
     </div>
   );
 };
