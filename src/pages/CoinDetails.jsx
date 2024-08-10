45959cd84ba6492158b44316bb9dcb0e2db58ddf
Marija Mitrovic <marija.mitrovic.frontend@gmail.com>
2024-08-10 17:29:11 +0200

    Change few things/ Add DarkMode in CoinDetails Page

--- src/pages/CoinDetails.jsx
+++ src/pages/CoinDetails.jsx
@@ -7,9 +7,15 @@ import { useQuery } from '@tanstack/react-query'
 import { fetchSingleCoin } from '@helpers/apiHelper.js'
 
 import Loader from "@components/Loader.jsx"
-import CoinChart from '@pages/CoinChart.jsx'
+import CoinChart from '@components/CoinChart.jsx'
+
+import { darkModeConfig } from "@helpers/zustandDarkMode.js";
+
 
 const CoinDetail = () => {
+
+  const { darkMode, setDarkMode } = darkModeConfig();
+
   const { id } = useParams();
 
   const { data, error, isPending } = useQuery({
@@ -18,23 +24,28 @@ const CoinDetail = () => {
   });
 
   return (
-    <div>
-      { error && <div> An error occured. Please refresh the page. </div>}
+    <div className={darkMode ? "CoinDetails dark" : "CoinDetails"}>
+<div className='coin-container'>
+  { error && <div> An error occured. Please refresh the page. </div>}
       { isPending && <Loader />}
       {data &&  (
-        <div>
-          <img src={data.image.large} alt={data.name} />
-          <h1>{data.name} ({data.symbol.toUpperCase()})</h1>
-          <p> <CoinChart /></p>
-          <p>Crypto Market Rank: {data.market_cap_rank}</p>
-          <p>Current Price: ${data.market_data.current_price.usd.toLocaleString()}</p>
-          <p>Market Cap: ${data.market_data.market_cap.usd.toLocaleString()}</p>
-          <p>24h Change: {data.market_data.price_change_percentage_24h.toLocaleString()}%</p>
-          <p>24h high: ${data.market_data.high_24h.usd.toLocaleString()}</p>
-          <p>24h low : ${data.market_data.low_24h.usd.toLocaleString()}</p>
+        <div className='coin-details-card'>
+          <img className='coinImage'  src={data.image.large} alt={data.name} />
+          <h1 className='cointitle'>{data.name} ({data.symbol.toUpperCase()})</h1>
+          <p className='coin-chart'> <CoinChart /></p>
+          <div className='coin-info'>
+          <p><span>Crypto Market Rank:</span> {data.market_cap_rank}</p>
+          <p><span>Current Price:</span> ${data.market_data.current_price.usd.toLocaleString()}</p>
+          <p><span>Market Cap:</span> ${data.market_data.market_cap.usd.toLocaleString()}</p>
+          <p><span>24h Change:</span> {data.market_data.price_change_percentage_24h.toLocaleString()}%</p>
+          <p><span>24h high:</span> ${data.market_data.high_24h.usd.toLocaleString()}</p>
+          <p><span>24h low:</span> ${data.market_data.low_24h.usd.toLocaleString()}</p>
+          </div>
           </div>
       )}
+  </div>
     </div>
+    
   );
 };
 
