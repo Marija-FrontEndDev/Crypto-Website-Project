bcba77cb284f5b0483e50bace7f33bdb2b793523
Marija Mitrovic <marija.mitrovic.frontend@gmail.com>
2024-08-10 02:50:53 +0200

    Change few thins/ Add DarkMode on LogIn page

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -3,28 +3,31 @@ import "@styles/LogIn.scss";
 import { useState } from 'react'
 import { Link } from "react-router-dom";
 
-const LogIn = () => {
+import { darkModeConfig } from "@helpers/zustandDarkMode.js";
 
+const LogIn = () => {
+ 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [emailError, setEmailError] = useState('');
   const [passwordError, setPasswordError] = useState('');
+  const { darkMode, setDarkMode } = darkModeConfig();
 
   const validateEmail = (email) => {
-    const emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/ ;
+    const emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
     return emailPattern.test(email);
   };
 
   const validatePassword = (password) => {
-  const passwordPattern= /(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}/;
-  return passwordPattern.test(password);
+    const passwordPattern = /(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}/;
+    return passwordPattern.test(password);
   };
 
   const handleLogin = (e) => {
     e.preventDefault();
     setEmailError('');
     setPasswordError('');
-    
+
     if (!validateEmail(email)) {
       setEmailError('Invalid email address.');
       return;
@@ -34,18 +37,19 @@ const LogIn = () => {
       setPasswordError('Password must be at least 8 characters long, include one uppercase letter, one lowercase letter, and one symbol.');
       return;
     }
-
-  };
+    }
 
   return (
-    <div className='card-container'>
+    <div className={darkMode ? "Login dark" : "Login"}>
+<div className='card-container'>
 <div className='log-in-container'>
       <h1 className='welcome'>Welcome Back!</h1>
-      <p>Log in to continue.</p>
+      <p className='logpar'>Log in to continue.</p>
       <form onSubmit={handleLogin} noValidate>
         <div >
           <label htmlFor="email" >Email: </label>
           <input
+          className='inputField'
           placeholder='Type your email'
             type="email"
             id="email"
@@ -58,6 +62,7 @@ const LogIn = () => {
         <div>
           <label htmlFor="password">Password:</label>
           <input
+          className='inputField'
           placeholder='Type your password'
             type="password"
             id="password"
@@ -69,12 +74,14 @@ const LogIn = () => {
         </div>
         <button type="submit" className='loginbutton'>Log In</button>
       </form>
-      <p> Don't have an account?{' '}
+      <p className='logpar'> Don't have an account?{' '}
         <button> <Link to="/get-started">Sign Up</Link></button>
       </p>
     </div>
     </div>
   
+    </div>
+    
   );
 };
 
